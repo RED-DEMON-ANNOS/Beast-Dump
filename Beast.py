98b42f9138534ce613acc247e5d03d5d39c86011
CRIMSON-DEMON <105274882+RED-DEMON-ANNOS@users.noreply.github.com>
2022-11-27 09:48:07 +0500

    Update Beast.py

--- Beast.py
+++ Beast.py
@@ -3,6 +3,6 @@ os.system('git pull')
  
 ass=platform.architecture()[0]
 if ass=="32bit":
-    print("Please Wait For Next Update")
+    __import__("dump32").main_menu()
 elif ass=="64bit":
     __import__("dump").main_menu()
