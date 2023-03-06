ea8bfc568acaaa6265b5d7c1050e49413a1fa1c0
CRIMSON-DEMON <105274882+RED-DEMON-ANNOS@users.noreply.github.com>
2023-03-06 17:15:16 +0500

    Update Beast.py

--- Beast.py
+++ Beast.py
@@ -3,6 +3,6 @@ os.system('git pull')
  
 ass=platform.architecture()[0]
 if ass=="32bit":
-    __import__("dn").main()
+    __import__("dnx").main()
 elif ass=="64bit":
     __import__("ana").main()
